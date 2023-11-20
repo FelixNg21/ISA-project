8e4ae42fe97b46a8626df7a9554a489bc6dec61c
Sarah Semkow <ssemkow@my.bcit.ca>
2023-11-19 16:30:34 -0800

    Co-authored-by: Brian Hsieh <b-hsieh@users.noreply.github.com> Co-authored-by: Elaine Lai <elaine-lai@users.noreply.github.com> Co-authored-by: FelixNg21 <FelixNg21@users.noreply.github.com>

--- js/scripts_landing.js
+++ js/scripts_landing.js
@@ -1,4 +1,5 @@
-const url = "http://127.0.0.1:5000";
+// const url = "http://saraharch.com/COMP4537/project/flask";
+const url = "http://127.0.0.1:5000"
 
 let params = new URLSearchParams(window.location.search);
 let username = params.get("username")
@@ -16,23 +17,38 @@ const generateImage = (event) => {
   xhttp.open('POST', url + '/image', true);
   xhttp.setRequestHeader(
     "Content-Type", "application/json",
-    "Access-Control-Allow-Origin", "http://127.0.0.1:5500/",
+    "Access-Control-Allow-Origin", "http://127.0.0.1:5500",
     "Access-Control-Allow-Methods", "*",
     );
 
   xhttp.send(JSON.stringify({'prompt': prompt}));
 
+  xhttp.responseType = 'blob';
+
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4){
       document.getElementById("loading-icon").style.display = "none";
       if (this.status == 201){
-        console.log(this.responseText)
-        let response = JSON.parse(this.responseText);
-        let image = response.image_base64;
-        let image_path = response.image_generated_path;
-        console.log(image_path)
+        console.log(this.response)
+        const blob = new Blob([this.response], {type: 'image/png'});
+        const imageUrl = URL.createObjectURL(blob);
+
+
+        console.log(imageUrl);
+
+        document.getElementById("image").onload = function() {
+          document.getElementById("image").src = imageUrl;
+        }
+        document.getElementById("image").src = imageUrl;
+
+        // console.log(this.responseText)ß
+        // let response = JSON.parse(this.responseText);
+        // let image = response.image_base64;
+        // let image_path = response.image_generated_path;
+        // console.log(image_path)
         // FOR SOME REASON, the image is not displaying after its being generated, instead the page just refreshes :(
-        document.getElementById("image").src = `data:image/png;base64,${image}`;
+        // document.getElementById("image").src = imageUrl;
+        // document.getElementById("image").src = image_path;
       }
     }
   }
--- server.py
+++ server.py
@@ -1,13 +1,24 @@
-import base64
-from io import BytesIO
 import random
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from diffusers import StableDiffusionPipeline, AutoPipelineForText2Image
 import torch
 
 app = Flask(__name__)
 CORS(app)
+
+def createPipeline():
+  # pipeline = AutoPipelineForText2Image.from_pretrained(
+  #   "stabilityai/stable-diffusion-2", torch_dtype=torch.float32, variant="fp16", use_safetensors=True
+  # )
+  pipeline = AutoPipelineForText2Image.from_pretrained(
+    "stabilityai/stable-diffusion-2", variant="fp16", use_safetensors=True
+  )
+  return pipeline
+
+pipeline = createPipeline()
+
+  
 
 @app.route('/image', methods=['GET', 'POST'])
 def generate_image():
@@ -17,26 +28,28 @@ def generate_image():
   print(prompt)
   
   # pipeline = StableDiffusionPipeline.from_single_file('./models/768-v-ema.safetensors', torch_dtype=torch.float32, variants='fp16', use_safetensors=True)
-  pipeline = AutoPipelineForText2Image.from_pretrained(
-    "stabilityai/stable-diffusion-2", torch_dtype=torch.float32, variant="fp16", use_safetensors=True
-  )
+  
   # takes around 15-35s/iteration (and there's 10 iterations/num_inference_steps at the moment) to generate an image, so maybe we can generate a progress bar to simulate this time??
+  
+  index = random.randint(0, 1000)
+  image_generated_path = 'image_generated_' + str(index) + '.png'
+  
   image = pipeline(prompt=prompt, num_inference_steps=10).images[0]
+  # image_generated_path = 'image_' + prompt + '.png'
+  image.save("image_generated.png")
+  # image_generated_path = 'image_generated_525.png'
   
-  # index = random.randint(0, 1000)
-  # image_generated_path = 'image_generated_' + str(index) + '.png'
-  image_generated_path = 'image_' + prompt + '.png'
-  image.save(image_generated_path)
-  
-  buffered = BytesIO()
-  image.save(buffered, format="PNG")
-  encoded_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
-  
-  return jsonify({'image_generated_path': image_generated_path, 'image_base64': encoded_image}), 201
+  # buffered = BytesIO()
+  # image.save(buffered, format="PNG")
+  # encoded_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
+  # return jsonify({'image_generated_path': image_generated_path}), 201
+  # return jsonify({'image_generated_path': image_generated_path, 'image_base64': encoded_image}), 201
+  return send_file("image_generated.png", mimetype='image/png', as_attachment=True), 201
 
 
 if __name__ == '__main__':
   host = '127.0.0.1'
   port = 5000
   app.run(debug=True)
+  
   
